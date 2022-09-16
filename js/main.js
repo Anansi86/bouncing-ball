0519559bf0093c375282c6f0edfa9098da9575ac
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-09-16 13:39:34 +0000

    canvas

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const canva = document.querySelector('canvasd');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
